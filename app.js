a834a6bc63fa2e9c5252b68041d5bcb871b4d485
Mathias Podesta <44898573+mapodesta@users.noreply.github.com>
2022-06-20 18:49:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const fs = require('fs');
 
-// create our express app
+
 const app = express()
 // middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 // route
 const routes = require('./Routes/Route')
 app.use('/', routes)
