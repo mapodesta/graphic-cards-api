8c5ef9b5b0d321bbc9ba9839c6cd50862f6f8116
mapodesta <mathias643@gmail.com>
2022-06-20 19:05:33 +0100

    change-port

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
-
-const app = express()
+const app = express();
 // middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // route
-const routes = require('./Routes/Route')
-app.use('/', routes)
+const routes = require("./Routes/Route");
+app.use("/", routes);
 
 //start server
-app.listen(3000, ()=>{
-    console.log("listeniing at port:3000")
-}) 
+app.listen(process.env.PORT || 5000, () => {
+  console.log("listeniing at port:3000");
+});
